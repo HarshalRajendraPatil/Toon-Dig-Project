1df8b54b2e4b3b9d80eb84ec392ce27a27bd07e8
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2024-11-27 22:58:43 +0530

    Update axiosConfig.js

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://https://toondig-backend.onrender.com", // API base URL
+  baseURL: "https://toondig-backend.onrender.com", // API base URL
   timeout: 20000, // Request timeout (10 seconds)
   headers: {
     "Content-Type": "application/json", // Default headers
